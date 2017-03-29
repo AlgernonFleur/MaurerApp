cd2ad23a1da484b2d12ec2a785369c694e58f283
AlgernonFleur <algernon.fleur@gmail.com>
2017-03-29 16:33:18 +1100

    Initial commit

---
+++ src/shapes/Point.java
@@ -0,0 +1,19 @@
+package shapes;
+
+/**
+ * Created by Algernon on 29/03/2017.
+ */
+public class Point {
+	private float x,y;
+	
+	public Point(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX() {return x;}
+	public void setX(float x) {this.x = x;}
+	
+	public float getY() {return y;}
+	public void setY(float y) {this.y = y;}
+}
