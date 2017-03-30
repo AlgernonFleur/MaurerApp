c001ca7f6a46d1cd56058da00e2ee2fe2381ef42
AlgernonFleur <algernon.fleur@gmail.com>
2017-03-30 18:53:31 +1100

    Yay i didn't botch up the first bit of implementation

--- src/gui/MaurerApp.java
+++ src/gui/MaurerApp.java
@@ -5,19 +5,24 @@ import controlP5.ControlFont;
 import controlP5.ControlListener;
 import controlP5.ControlP5;
 import processing.core.PApplet;
+import shapes.MaurerRose;
+import shapes.Point;
 
 /**
  * Created by Algernon on 30/03/2017.
  */
 public class MaurerApp extends PApplet {
 	
+	MaurerRose rose;
 	private ControlP5 ctrl;
 	private int nVal = 1, dVal = 1; //default values
 	
 	public void setup(){
+		rose = new MaurerRose(nVal,dVal);
 		ctrl = new ControlP5(this);
 		ctrl.setFont(new ControlFont(createFont("Arial",20)));
 		ButtonListener listener = new ButtonListener();
+		stroke(200);
 		
 		//Buttons to control N Value
 		ctrl.addButton("Inc N")
@@ -55,6 +60,7 @@ public class MaurerApp extends PApplet {
 			}
 			nVal = constrain(nVal,1,100);
 			dVal = constrain(dVal,1,100);
+			rose.update(nVal,dVal);
 		}
 	}
 	
@@ -62,6 +68,12 @@ public class MaurerApp extends PApplet {
 		background(50);
 		text("N value: " + nVal,0,200);
 		text("D value: " + dVal,105,200);
+		pushMatrix();
+		translate(width/2,height/2);
+		for (Point p:rose.getPointsList()) {
+			point((float)p.x(),(float)p.y());
+		}
+		popMatrix();
 	}
 	
 	public void settings(){fullScreen();}
