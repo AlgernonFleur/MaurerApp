ec7b85327f4771b8714fe600def6ee988c0a2f0b
AlgernonFleur <algernon.fleur@gmail.com>
2017-03-30 16:50:18 +1100

    Added gui base class to work on

---
+++ src/gui/MaurerApp.java
@@ -0,0 +1,21 @@
+package gui;
+
+import processing.core.PApplet;
+
+/**
+ * Created by Algernon on 30/03/2017.
+ */
+public class MaurerApp extends PApplet {
+	public void setup(){
+	
+	}
+	public void draw(){
+	
+	}
+	public void settings(){
+		fullScreen();
+	}
+	public static void main(String[] args) {
+		PApplet.main(MaurerApp.class.getName());
+	}
+}
